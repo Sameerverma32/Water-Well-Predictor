a5bd4ac663ae67eb6184339db792a36b72873f66
Sameerverma32 <sameerverma8318@gmail.com>
2026-04-10 06:15:05 +0530

    ml

--- app.py
+++ app.py
@@ -28,7 +28,11 @@ class User(db.Model):
     
     password = db.Column(db.String(150), nullable=False)
 
+# Create all database tables
+with app.app_context():
+    db.create_all()
 
+df = pd.read_csv('groundwater_ml_dataset_cleaned.csv')
 
 @app.route('/')
 def home():
@@ -45,6 +49,68 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+#graphs functions
+def Annual_Recharge_Trend():
+    fig = px.line(df, x="year", y="annual_recharge", color="state",
+            title="Annual Recharge Trend")
+    graph1_html = pio.to_html(fig, full_html=False)
+    return graph1_html
+
+def Statewise_Annual_Recharge():
+    fig = px.bar(df, x="state", y="annual_recharge",
+            title="State-wise Annual Recharge")
+    graph2_html = pio.to_html(fig, full_html=False)
+    return graph2_html
+
+def Annual_Extraction_Trend():
+    fig = px.line(df, x="year", y="annual_extraction", color="state",
+            title="Annual Extraction Trend")
+    graph3_html = pio.to_html(fig, full_html=False)
+    return graph3_html
+
+def Statewise_Annual_Extraction():
+    fig = px.bar(df, x="state", y="annual_extraction",
+            title="State-wise Annual Extraction")   
+    graph4_html = pio.to_html(fig, full_html=False)
+    return graph4_html
+
+def Recharge_vs_Extraction():
+    fig = px.scatter(df, x="annual_recharge", y="annual_extraction",
+                color="state",
+                title="Recharge vs Extraction")
+    graph5_html = pio.to_html(fig, full_html=False)
+    return graph5_html
+
+def Resource_Distribution():
+    fig = px.box(df, x="state", y="extractable_resource",
+            title="Resource Distribution")
+    graph6_html = pio.to_html(fig, full_html=False)
+    return graph6_html
+
+def Resource_Over_Time():
+    fig = px.area(df, x="year", y="extractable_resource",
+            title="Resource Over Time")
+    graph7_html = pio.to_html(fig, full_html=False)
+    return graph7_html
+
+def Recharge_Distribution():
+    fig = px.histogram(df, x="annual_recharge",
+                title="Recharge Distribution")
+    graph8_html = pio.to_html(fig, full_html=False)
+    return graph8_html
+
+def Category_Distribution():
+    fig = px.pie(df, names="category",
+            title="Category Distribution")
+    graph9_html = pio.to_html(fig, full_html=False)
+    return graph9_html
+
+
+
+
+
+
 
 if __name__ == '__main__':
     if not os.path.exists('users.db'):
