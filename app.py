7072d9181d1f9676b146de494f5da32619fa4aa4
Sameerverma32 <sameerverma8318@gmail.com>
2026-04-14 15:07:24 +0530

    ml

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-import plotly.figure_factory as ff
 
 
 app = Flask(__name__)
